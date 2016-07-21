daee0bef6b4034ca18d7c193a0bd565c655451d8
= <=>
2016-07-22 00:32:55 +0200

    seems to work with sox recording to a file

--- james.js
+++ james.js
@@ -1,18 +1,41 @@
 var witToken = "ITFQFWF2B2RPTC473JQG2SK7ITX7UZ3S";
  
-var Speakable = require('./node-speakable');
+var Speakable = require('./node-speakable'),
+    request = require('request'),
+    fs = require('fs');
 
 
-// Setup google speech
+
 var speakable = new Speakable();
 
 speakable.on('speechStart', function() {
   console.log('onSpeechStart');
 });
 
-speakable.on('speechStop', function() {
+speakable.on('speechStop', function(soundFileStream) {
   console.log('onSpeechStop');
-  speakable.recordVoice();
+  var source = fs.createReadStream('./recording.wave');
+
+  source.pipe(request.post(  
+    {
+      url: 'https://api.wit.ai/speech', //URL to hit
+      qs: {}, //Query string data
+      method: 'POST', //Specify the method
+      headers: { //We can define headers too
+          'Authorization': 'Bearer ' + process.env.WITAITOKEN,
+          'Content-Type': 'audio/wav',
+          'Transfer-encoding': 'chunked'
+      }
+    }, 
+    function(error, response, body){
+      if(error) {
+        console.log(error);
+      } else {
+        console.log(response.statusCode, body);
+      }
+    }
+  ));
+  //speakable.recordVoice();
 });
 
 speakable.on('speechReady', function() {
@@ -22,12 +45,7 @@ speakable.on('speechReady', function() {
 speakable.on('error', function(err) {
   console.log('onError:');
   console.log(err);
-  speakable.recordVoice();
-});
-
-speakable.on('speechResult', function(spokenWords) {
-  console.log('onSpeechResult:')
-  console.log(spokenWords);
+  //speakable.recordVoice();
 });
 
 speakable.recordVoice();
--- node-speakable.js
+++ node-speakable.js
@@ -1,52 +1,39 @@
 var EventEmitter = require('events').EventEmitter,
     util = require('util'),
     spawn = require('child_process').spawn,
+    MemoryStream = require('memorystream'),
+    request = require('request'),
+    fs = require('fs'),
     http = require('http');
+
+var soundFilePath = "./recording.wav";
 
 var Speakable = function Speakable(options) {
   EventEmitter.call(this);
 
+//TODO: add default settings.
   options = options || {}
 
-  this.recBuffer = [];
-  this.recRunning = false;
-  this.apiResult = {};
-  this.apiLang = options.lang || "en-US";
+  this.writeStream = null;
   this.cmd = 'sox';
   this.cmdArgs = [
     '-q',
+    '-c', '1',
     '-b','16',
-    '-d','-t','flac','-',
+    '-d',
+    '-t','wav',
+    'recording.wave',
     'rate','16000','channels','1',
-    'silence','1','0.1',(options.threshold || '0.1')+'%','1','1.0',(options.threshold || '0.1')+'%'
+    'silence','1','0.1',(options.threshold || '3')+'%','1','1.0',(options.threshold || '3')+'%'
   ];
-
 };
 
 util.inherits(Speakable, EventEmitter);
-module.exports = Speakable;
 
 Speakable.prototype.recordVoice = function() {
   var self = this;
 
   var rec = spawn(self.cmd, self.cmdArgs);
-
-  // Process stdout
-
-  rec.stdout.on('readable', function() {
-    self.emit('speechReady');
-  });
-
-  rec.stdout.setEncoding('binary');
-  rec.stdout.on('data', function(data) {
-    if(! self.recRunning) {
-      self.emit('speechStart');
-      self.recRunning = true;
-    }
-    self.recBuffer.push(data);
-  });
-
-  // Process stdin
 
   rec.stderr.setEncoding('utf8');
   rec.stderr.on('data', function(data) {
@@ -54,25 +41,13 @@ Speakable.prototype.recordVoice = function() {
   });
 
   rec.on('close', function(code) {
-    self.recRunning = false;
     if(code) {
       self.emit('error', 'sox exited with code ' + code);
+    }else{
+      self.emit('speechStop',soundFilePath);
     }
-    self.emit('speechStop');
   });
 };
 
-Speakable.prototype.resetVoice = function() {
-  var self = this;
-  self.recBuffer = [];
-}
+module.exports = Speakable;
 
-Speakable.prototype.parseResult = function() {
-  var recognizedWords = [], apiResult = this.apiResult;
-  if(apiResult.hypotheses && apiResult.hypotheses[0]) {
-    recognizedWords = apiResult.hypotheses[0].utterance.split(' ');
-    this.emit('speechResult', recognizedWords);
-  } else {
-    this.emit('speechResult', []);
-  }
-}
